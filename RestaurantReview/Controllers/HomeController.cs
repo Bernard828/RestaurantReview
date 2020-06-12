b505b97d22a9fc455ba7b58720fd0cd21c33998d
Dakota <Dakotawhittle1203@gmail.com>
2020-06-12 13:17:19 -0400

    created wwwroot folder and contents, Interface

--- RestaurantReview/Controllers/HomeController.cs
+++ RestaurantReview/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using RestaurantReview.Models;
+using RestaurantReview.Repositories;
 
 namespace RestaurantReview.Controllers
 {
@@ -12,7 +14,9 @@ namespace RestaurantReview.Controllers
 
         public ViewResult Index()
         {
-            Restaurant model = new Restaurant("Donato's", 99, "Pretty decent pizza");
+            //Restaurant model = new Restaurant("Donato's", 99, "Pretty decent pizza");
+            RestaurantRepository restaurantRepository = new RestaurantRepository();
+            var model = restaurantRepository.GetAll();
             return View(model);
         }
     }
---
+++ RestaurantReview/Repositories/RestaurantRepository.cs
@@ -0,0 +1,26 @@
+﻿using RestaurantReview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace RestaurantReview.Repositories
+{
+    public class RestaurantRepository
+    {
+        public List<RestaurantReview.Models.Restaurant> restaurants;
+
+        public RestaurantRepository()
+        {
+            restaurants = new List<Models.Restaurant>();
+            restaurants.Add(new Models.Restaurant("Donato's", 1, "Great pizza place"));
+            restaurants.Add(new Models.Restaurant("Agave", 1, "Like chipotle but 10x better"));
+            restaurants.Add(new Models.Restaurant("Pier W", 1, "Great seafood"));
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants;
+        }
+    }
+}
