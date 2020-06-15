192051da0497ce4b8abe25de44dfc5d75aa509a8
Corey T Cox <coxcorey19@gmail.com>
2020-06-15 14:10:32 -0400

    added linkImage linkUrl and linkDescription properties to Restaurant class

--- RestaurantReview/Models/Restaurant.cs
+++ RestaurantReview/Models/Restaurant.cs
@@ -7,11 +7,14 @@ namespace RestaurantReview.Models
 {
     public class Restaurant
     {//Course model = new Course("Intro to MVC", 42, "All you need to know about MVC");
-        public Restaurant(string name, int id, string details)
+        public Restaurant(string name, int id, string details, string linkImage, string linkUrl, string linkDescription)
         {
             this.Name = name;
             this.Id = id;
             this.Details = details;
+            this.LinkImage = linkImage;
+            this.LinkUrl = linkUrl;
+            this.LinkDescription = linkDescription;
         }
         public string Name { get; set; }
         public int Id { get; set; }
