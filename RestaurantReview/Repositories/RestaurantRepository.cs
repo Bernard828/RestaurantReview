9ab8619f04c09eb1b8a110104f5cf584acdadfdc
Corey T Cox <coxcorey19@gmail.com>
2020-06-15 14:46:12 -0400

    finished adding Link properties

--- RestaurantReview/Repositories/RestaurantRepository.cs
+++ RestaurantReview/Repositories/RestaurantRepository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
 using System.Threading.Tasks;
 
 namespace RestaurantReview.Repositories
@@ -14,9 +15,9 @@ namespace RestaurantReview.Repositories
         public RestaurantRepository()
         {
             restaurants = new List<Models.Restaurant>();
-            restaurants.Add(new Models.Restaurant("Donato's", 1, "Great pizza place"));
-            restaurants.Add(new Models.Restaurant("Agave", 1, "Like chipotle but 10x better"));
-            restaurants.Add(new Models.Restaurant("Pier W", 1, "Great seafood"));
+            restaurants.Add(new Models.Restaurant("Donato's", 1,"Great pizza place", "images/Donatos-Pizza-restaurant-1200x900.jpg", "https://www.donatos.com", "Donatos Image"));
+            restaurants.Add(new Models.Restaurant("Agave", 2, "Like chipotle but 10x better"), image, https://www.facebook.com/agaveburritobar/ , "Agave Image);
+            restaurants.Add(new Models.Restaurant("Pier W", 3, "Great seafood", image, http://pierw.com/, "Pier W Image");
         }
         public IEnumerable<Restaurant> GetAll()
         {
