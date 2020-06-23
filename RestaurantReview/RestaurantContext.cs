4578e1163655f7d88378b906a6820eb674630491
Corey T Cox <coxcorey19@gmail.com>
2020-06-23 12:00:05 -0400

    changed restaurant details and added reviews

--- RestaurantReview/RestaurantContext.cs
+++ RestaurantReview/RestaurantContext.cs
@@ -30,21 +30,18 @@ namespace RestaurantReview
                 {
                     Name = "Donato's",
                     Id = 1,
-                    Details = "Great pizza place",
+                    Details = "Donatos is a family-owned pizza company famous for Edge to Edge® toppings on a crispy golden thin crust. Founded in Columbus, Ohio in 1963, we take pride in giving back in every community we serve.",
                     LinkImage = "./images/Donatos-Pizza-restaurant-1200x900.jpg",
                     LinkUrl = "https://www.donatos.com",
                     LinkDescription = "Donatos Image"
 
-                    //restaurants.Add(new Models.Restaurant("Donato's", 1,"Great pizza place", "./images/Donatos-Pizza-restaurant-1200x900.jpg", "https://www.donatos.com", "Donatos Image"));
-                    //restaurants.Add(new Models.Restaurant("Agave", 2, "Like chipotle but 10x better", "./images/agave.jpg", "https://www.facebook.com/agaveburritobar/", "Agave Image"));
-                    //restaurants.Add(new Models.Restaurant("Pier W", 3, "Great seafood", "./images/pierW.jpg", "http://pierw.com/", "Pier W Image"));
                 },
 
                 new Restaurant()
                 {
                     Name = "Agave",
                     Id = 2,
-                    Details = "Like chipotle but 10x better",
+                    Details = "An Oberlin staple since 2003, Agave is the spot for fresh Mexi-Cali quick-serve cuisine, exceptional margaritas and draft beer!",
                     LinkImage = "./images/agave.jpg",
                     LinkUrl = "https://www.facebook.com/agaveburritobar/",
                     LinkDescription = "Agave Image"
@@ -55,7 +52,7 @@ namespace RestaurantReview
                  {
                      Name = "Pier W",
                      Id = 3,
-                     Details = "Really great seafood",
+                     Details = "Pier W is a landmark restaurant that has been Cleveland's premier destination for five-star dining, fresh seafood and fine wines since 1965.",
                      LinkImage = "./images/pierW.jpg",
                      LinkUrl = "http://pierw.com/",
                      LinkDescription = "Pier W Image",
@@ -67,7 +64,7 @@ namespace RestaurantReview
                     Id = 1,
                     RestaurantId = 1,
                     Creator = "Bernard",
-                    Comment = " Pepporoni Pizza is awesome",
+                    Comment = "Pepporoni Pizza is awesome. The lady at the counter was great! She tossed in some extra breadsticks because they were honoring the community that day! Thanks again Donato's i'll be back!!!",
                     ReviewDate = DateTime.Now
                 },
                  new Review()
@@ -75,15 +72,14 @@ namespace RestaurantReview
                      Id = 2,
                      RestaurantId = 1,
                      Creator = "Brenda",
-                     Comment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est quam, ornare sit amet pretium non, posuere rhoncus dui. Curabitur gravida risus eget risus scelerisque, eu tristique velit accumsan. Nam ligula purus, sagittis sed dolor vitae, posuere accumsan purus. Donec metus velit, commodo eu viverra in, lobortis ac diam. Suspendisse lacinia mi mauris, at ultrices dolor maximus eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos",
-                     ReviewDate = DateTime.Now
+                     Comment = "Whether you are traveling thru Ohio, or you live here, you have to try Donatos Pizza!Last year, I gave up on pizza, as I found all the chains to be measly with their veggies, heavy on the salt, and not so fresh. But now that I've eaten Donatos, I've changed my mind completely. Tho I order my pizza as 'takeout', the store had a nice eating area with booths. It was very clean and tidy, and it offers salads and sides. Wholesome atmosphere."
                  },
                 new Review()
                 {
                     Id = 3,
                     RestaurantId = 1,
                     Creator = "Corey",
-                    Comment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est quam, ornare sit amet pretium non, posuere rhoncus dui. Curabitur gravida risus eget risus scelerisque, eu tristique velit accumsan. Nam ligula purus, sagittis sed dolor vitae, posuere accumsan purus. Donec metus velit, commodo eu viverra in, lobortis ac diam. Suspendisse lacinia mi mauris, at ultrices dolor maximus eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos",
+                    Comment = "Hopefully the previous comments got this place in 5 star shape because I just ordered for the first time and it is delicious! I ordered a large create your own with well done instructions and it came early and perfectly done. Glad I gave them a chance. The delivery gal was super friendly too!",
                     ReviewDate = DateTime.Now
                 },
                 new Review()
@@ -91,7 +87,7 @@ namespace RestaurantReview
                     Id = 4,
                     RestaurantId = 2,
                     Creator = "Laurie",
-                    Comment = " Their burritos were great!",
+                    Comment = "We were in town from out of state and decided to stop in for dinner. Fun atmosphere, amazing food and drinks ( loved the Pink Dragon!) I would definitely come back if I’m ever in the area. The 2 people working there were very friendly and did their job well and with smiles on their faces adding to the great atmosphere!",
                     ReviewDate = DateTime.Now
                 },
 
@@ -100,7 +96,7 @@ namespace RestaurantReview
                     Id = 5,
                     RestaurantId = 2,
                     Creator = "Jay",
-                    Comment = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est quam, ornare sit amet pretium non, posuere rhoncus dui. Curabitur gravida risus eget risus scelerisque, eu tristique velit accumsan. Nam ligula purus, sagittis sed dolor vitae, posuere accumsan purus. Donec metus velit, commodo eu viverra in, lobortis ac diam. Suspendisse lacinia mi mauris, at ultrices dolor maximus eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos",
+                    Comment = "This place might have ruined Chipotle for me. The food here is so amazing I don’t want any Mexican anywhere else again!",
                     ReviewDate = DateTime.Now
                 },
                 new Review()
@@ -108,7 +104,7 @@ namespace RestaurantReview
                     Id = 6,
                     RestaurantId = 2,
                     Creator = "Josh",
-                    Comment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est quam, ornare sit amet pretium non, posuere rhoncus dui. Curabitur gravida risus eget risus scelerisque, eu tristique velit accumsan. Nam ligula purus, sagittis sed dolor vitae, posuere accumsan purus. Donec metus velit, commodo eu viverra in, lobortis ac diam. Suspendisse lacinia mi mauris, at ultrices dolor maximus eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos",
+                    Comment = "We love Agave! Very family friendly, great prices, great food and nice staff! Highly recommend their queso and chips! This is a fast casual type of restaurant.",
                     ReviewDate = DateTime.Now
                 },
                 new Review()
@@ -116,7 +112,7 @@ namespace RestaurantReview
                     Id = 7,
                     RestaurantId = 3,
                     Creator = "Corey",
-                    Comment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est quam, ornare sit amet pretium non, posuere rhoncus dui. Curabitur gravida risus eget risus scelerisque, eu tristique velit accumsan. Nam ligula purus, sagittis sed dolor vitae, posuere accumsan purus. Donec metus velit, commodo eu viverra in, lobortis ac diam. Suspendisse lacinia mi mauris, at ultrices dolor maximus eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos",
+                    Comment = "Surprisingly, there are not many Cleveland restaurants with a good lake view, but Pier W has the best. Plus, the selections of meals for lunch are outstanding. The service was excellent and meals reasonable and filling. We enjoyed perch, salmon, scallops, The lobster bisque and manilla clam chowder were equally tasty. Our guests left satisfied and impressed with their visit to Lakewood and Pier W.",
                     ReviewDate = DateTime.Now
                 },
                 new Review()
@@ -124,7 +120,7 @@ namespace RestaurantReview
                     Id = 8,
                     RestaurantId = 3,
                     Creator = "John",
-                    Comment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est quam, ornare sit amet pretium non, posuere rhoncus dui. Curabitur gravida risus eget risus scelerisque, eu tristique velit accumsan. Nam ligula purus, sagittis sed dolor vitae, posuere accumsan purus. Donec metus velit, commodo eu viverra in, lobortis ac diam. Suspendisse lacinia mi mauris, at ultrices dolor maximus eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos",
+                    Comment = "Ventured to Pier W for dinner on our 22nd wedding anniversary. We were seated quickly with a waterview despite our running late. Our server was polite, knowledgeable & helpful. Our ceviche appetizer was just so-so, but the lobster bisque was delicious. Our dinners consisted of lobster pappardelle & bouillabaisse. The lobster pappardelle was done in a cream sauce that was well- balanced. The bouillabaisse was outstanding; loaded with scallops, muscles, shrimp, scallops & fish. The accompanying broth was fantastic & didn't overpower the seafood. We received a complimentary cheesecake for our anniversary that was very good & the wife took a serving of Buttercake home for good measure... and the next morning's snack.",
                     ReviewDate = DateTime.Now
                 },
                 new Review()
@@ -132,7 +128,7 @@ namespace RestaurantReview
                     Id = 9,
                     RestaurantId = 3,
                     Creator = "Dakota",
-                    Comment = "The seafood here is pretty ok for Lake Erie",
+                    Comment = "I am one who doesn't complain, but when I went to Pier W I witnessed an injustice that ruined my whole experience there. I witnessed the general manager. Scream at and berate a poor teenage busser for not moving a chair down to a table fast enough for his liking. Then the man noticed us watching him. He approached our table right by the stars. My girlfriend and I felt awkward after seeing this man throw a tempertantrum, come up to us calmly and ask us how our dinner is going. After a awkward conversation that lasted way to long with him, he walked away. 20 mins after he was screaming at someone in his headset. Nice restaurant, bad manager!!!",
                     ReviewDate = DateTime.Now,
                 });
 
