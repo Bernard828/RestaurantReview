deb36ab50bf93876784e9a1894d84ce488c7d381
unknown <coxcorey19@gmail.com>
2020-09-09 16:35:56 -0400

    forked repo

--- RestaurantReview.Test/HomeControllerTest.cs
+++ RestaurantReview.Test/HomeControllerTest.cs
@@ -2,26 +2,38 @@ using RestaurantReview.Controllers;
 using System;
 using Xunit;
 using Microsoft.AspNetCore.Mvc;
+using RestaurantReview.Models;
 
 namespace RestaurantReview.Test
 {
     public class HomeControllerTest
     {
-    //    [Fact]
-    //    public void Index_Returns_ViewResult()
-    //    {
-    //        var underTest = new RestaurantController();
-    //        var result = underTest.Index();
-    //        Assert.IsType<ViewResult>(result);
-    //    }
-    //    [Fact]
-    //public void Index_Passes_Restaurant_Model_Is_View()
-    //    {
-    //        var underTest = new RestaurantController();
+        //    [Fact]
+        //    public void Index_Returns_ViewResult()
+        //    {
+        //        var underTest = new RestaurantController();
+        //        var result = underTest.Index();
+        //        Assert.IsType<ViewResult>(result);
+        //    }
+        //    [Fact]
+        //public void Index_Passes_Restaurant_Model_Is_View()
+        //    {
+        //        var underTest = new RestaurantController();
 
-    //        var result = underTest.Index();
+        //        var result = underTest.Index();
 
-    //        Assert.IsType <RestaurantReview.Models.Restaurant>(result.Model);
-    //    }
+        //        Assert.IsType <RestaurantReview.Models.Restaurant>(result.Model);
+        //    }
+        //[Fact]
+        //public void Index_Sets_Name_On_Model()
+        //{
+        //    var expectedName = "ExampleString";
+        //    var controller = new RestaurantController();
+
+        //    var result = controller.Index(expectedName);
+
+        //    var model = (Restaurant)result.Model;
+        //    Assert.Equal(expectedName, model.Name);
+        //}
     } 
 }
